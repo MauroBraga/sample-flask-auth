a981eba0ed660ad9578b0b94d9579e529211bd8c
mauro <mrb0305@gmail.com>
2024-12-10 18:44:12 -0300

    Autenticação e FlaskLogin

--- app.py
+++ app.py
@@ -37,11 +37,60 @@ def login():
 
   return jsonify({"message": "Credenciais inválidas"}),400
 
+@app.route("/logout", methods=["GET"])
+@login_required
+def logout():
+  logout_user()
+  return jsonify({"message":"Logout realizado com sucesso"})
 
-@app.route("/hello-word", methods=["GET"])
-def hello_world():
-  return "Hello world"
+@app.route('/user', methods=["POST"])
+@login_required
+def create_user():
+  data = request.json
+  username = data.get("username")
+  password = data.get("password")
 
+  if username and password:
+    user = User(username=username,password=password)
+    db.session.add(user)
+    db.session.commit()
+    return jsonify({"message":"Usuário cadastrado com sucesso!"}),200
+
+  return jsonify({"message":"Dados inválidos!"}),400
+
+@app.route('/user/<int:id_user>', methods=["GET"])
+def findById(id_user):
+  user = User.query.get(id_user)
+  if user:
+    return {"username":user.username}
+  return jsonify({"message": "Usuario não encontrado"}), 404
+
+
+@app.route('/user/<int:id_user>', methods=["PUT"])
+def update(id_user):
+  data = request.json
+  user = User.query.get(id_user)
+  if user and data.get("password"):
+    user.password = data.get("password")
+    db.session.commit()
+    return jsonify({"message": f"Usuário {id_user} atualizado com sucesso"})
+  return jsonify({"message": "Usuario não encontrado"}), 404
+
+
+@app.route('/user/<int:id_user>', methods=["DELETE"])
+@login_required
+def delete_user(id_user):
+  user = User.query.get(id_user)
+
+  if id_user != current_user.id:
+    return jsonify({"message": "Deleção não permitida"}), 403
+
+  if user:
+    db.session.delete(user)
+    db.session.commit()
+    return jsonify({"message": f"Usuário {id_user} deletado com sucesso"})
+
+  return jsonify({"message": "Usuario não encontrado"}), 404
 
 if __name__ == '__main__':
   app.run(debug=True)
